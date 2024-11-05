0e320be9d22d50c0fe87071e6d6e56b288033770
jhsong1220 <jhsong1220@ajou.ac.kr>
2024-11-05 22:11:56 +0900

    Revert "UI 11/5 작업분"
    This reverts commit 879689ccd3530283ff89319ce81dcd2de4e9d555.

--- Assets/재혁 UIUX/MainMenu.cs
+++ Assets/재혁 UIUX/MainMenu.cs
@@ -1,17 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void OnClickGameStart()
+    // Start is called before the first frame update
+    void Start()
     {
-        SceneManager.LoadScene("GameSelect"); //게임 선택창으로 이동
-        Debug.Log("변환 완료");
+        
     }
 
-<<<<<<< Updated upstream
     // Update is called once per frame
     void Update()
     {
@@ -34,9 +32,6 @@ public class MainMenu : MonoBehaviour
     }
 
     public void OnclickQuit()
-=======
-    public void OnCLickQuit()  // 종료버튼 
->>>>>>> Stashed changes
     {
 #if UNITY_EDITOR
     UnityEditor.EditorApplication.isPlaying= false;
